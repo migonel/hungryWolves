189fc6a5a7fdf80b92b210895aa8b13588379e66
Lucas <migonel55@gmail.com>
2023-01-29 21:56:51 -0300

    Hungry wolves v0.1

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HungryWolves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Hungry Wolves!");
+        }
+    }
+}
